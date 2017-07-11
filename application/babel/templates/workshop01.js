5254bb58aa73b5462589529ed9e9aabb53fb04d5
Yahia Refaiea <YahiaRefaiea@gmail.com>
2017-07-11 19:38:39 +0300

    project is released and under testing...

--- application/babel/templates/workshop01.js
+++ application/babel/templates/workshop01.js
@@ -1,98 +1,94 @@
 $(document).ready(function() {
   
-  parent = ['title1', 'title2', 'heading1', 'heading2', 'subtitle', 'normal1', 'normal2']
+  //  WRAP EACH WORD WITH A SPAN
+  parents = ['title1', 'title2', 'heading1', 'heading2', 'subtitle', 'normal1', 'normal2']
   
-  $.each(parent, function(i, x) {
-    children = $('.workshop #'+x).children()
+  $.each(parents, function(i, parent) {
+    children = $('.workshop #'+parent).children()
     
     for (i = 0; i < children.length; i++) {
       child = $(children[i])
       words = child.text().split(' ')
-      
       child.empty()
       
-      $.each(words, function(i, y) {
-        child.append($('<span>').text(y))
+      $.each(words, function(i, word) {
+        child.append($('<span>').text(word))
       })
     }
   })
   
   
   
-  
-  /////////////////////////////////////////////////////////////
-  
-  
-  
-  
-  classes = [
-    'slide-0',
+  //  SLIDES SYSTEM
+  slides = [
+    'slide-first',
+    'slide-greeting',
+    
     'slide-1-1', 'slide-1-2', 'slide-1-3', 'slide-1-4', 'slide-1-5', 'slide-1-6',
     'slide-2-1', 'slide-2-2', 'slide-2-3', 'slide-2-4', 'slide-2-5',
-    'slide-2-6', 'slide-2-6-1', 'slide-2-6-2',
+    'slide-2-6--1', 'slide-birthday', 'slide-2-6--2', 'slide-2-6-1', 'slide-2-6--3', 'slide-2-6-2','slide-2-6--4',
     'slide-2-7', 'slide-2-7-1', 'slide-2-7-2', 'slide-2-7-3', 'slide-2-7-4',
     
     'slide-3-1', 'slide-3-2', 'slide-3-3', 'slide-3-4', 'slide-3-5',
+    
+    'slide-break',
+    
     'slide-4-1', 'slide-4-2', 'slide-4-3', 'slide-4-4',
     'slide-5-1', 'slide-5-2', 'slide-5-3', 'slide-5-4',
     
     'slide-6-1', 'slide-6-2', 'slide-6-3',
     
-    'slide-7-1', 'slide-7-2', 'slide-7-3', 'slide-7-4', 'slide-7-5', 'slide-7-6', 'slide-7-7'
+    'slide-assignment',
+    
+    'slide-7-1', 'slide-7-2', 'slide-7-3', 'slide-7-4', 'slide-7-5', 'slide-7-6', 'slide-7-7',
+    'slide-last'
   ]
   
-  
-  
-  
-  function nextSlide() {
+  function changeSlide() {
+    slide = $('#workshop01').attr('data-slide')
+    $('.'+slide).removeClass('active')
     
-    active = $('#workshop01').attr('data-slide')
-    $('.'+active).removeClass('active')
+    index = slides.indexOf(slide)
+    if(dir == 'next')
+      active = slides[index+1]
+    else if(dir == 'prev')
+      active = slides[index-1]
     
-    index = classes.indexOf(active)
+    $('#workshop01').attr('data-slide', active)
+    $('.'+active).addClass('active')
     
-    newActive = classes[index+1]
-    
-    $('#workshop01').attr('data-slide', newActive)
-    
-    $('.'+newActive).addClass('active')
-    
+    if (typeof cheers !== "undefined") clearTimeout(cheers)
+    if(active == 'slide-7-6') {
+      cheers = setTimeout( function() {
+        $('.slide-7-6').removeClass('active')
+        
+        $('#workshop01').attr('data-slide', 'slide-7-7')
+        $('.slide-7-7').addClass('active')
+      }, 120000)
+    }
   }
   
-  function previousSlide() {
-    
-    active = $('#workshop01').attr('data-slide')
-    $('.'+active).removeClass('active')
-    
-    index = classes.indexOf(active)
-    
-    newActive = classes[index-1]
-    
-    $('#workshop01').attr('data-slide', newActive)
-    
-    $('.'+newActive).addClass('active')
-  }
-  
-//  $(document).on('click', function() {
-//    
-//  })
+  $(document).on('click', function() {
+    dir = 'next'
+    changeSlide()
+  })
   
   $(document).keydown(function(e) {
-    next = 39, previous = 37
+    rightArrow = 39, leftArrow = 37
     
     switch (e.keyCode) {
-      case previous:
-        previousSlide();
-        console.log('prev')
-        e.preventDefault();
-        break;
+      case rightArrow:
+        dir = 'next'
+        changeSlide()
+        e.preventDefault()
+        break
       
-      case next:
-        nextSlide();
-        console.log('next')
-        e.preventDefault();
-        break;
+      case leftArrow:
+        dir = 'prev'
+        changeSlide()
+        e.preventDefault()
+        break
     }
-  });
+  })
   
 })
