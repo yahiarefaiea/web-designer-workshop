de54148bc2dd1d19731060cc8b82511cd97dcaf1
Yahia Refaiea <YahiaRefaiea@gmail.com>
2017-07-14 12:16:10 +0300

    workshop 01 update

--- application/babel/templates/workshop01.js
+++ application/babel/templates/workshop01.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
     
     'slide-1-1', 'slide-1-2', 'slide-1-3', 'slide-1-4', 'slide-1-5', 'slide-1-6',
     'slide-2-1', 'slide-2-2', 'slide-2-3', 'slide-2-4', 'slide-2-5',
-    'slide-2-6--1', 'slide-birthday', 'slide-2-6--2', 'slide-2-6-1', 'slide-2-6--3', 'slide-2-6-2','slide-2-6--4',
+    'slide-2-6--1', 'slide-2-6-1', 'slide-2-6--2', 'slide-2-6-2','slide-2-6--3',
     'slide-2-7', 'slide-2-7-1', 'slide-2-7-2', 'slide-2-7-3', 'slide-2-7-4',
     
     'slide-3-1', 'slide-3-2', 'slide-3-3', 'slide-3-4', 'slide-3-5',
@@ -64,7 +64,7 @@ $(document).ready(function() {
         
         $('#workshop01').attr('data-slide', 'slide-7-7')
         $('.slide-7-7').addClass('active')
-      }, 120000)
+      }, 30000)
     }
   }
   
